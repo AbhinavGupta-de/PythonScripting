3ce7696bb85f346922859c0bede1aa214988a308
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-10-19 17:11:50 +0530

    made some more small changes

---
+++ basicProblems/slicing.py
@@ -0,0 +1,12 @@
+# website_template = "https://domain-name"
+website_1 = "https://google.com"
+website_2 = "https://youtube.com"
+website_3 = "https://scaler.com"
+
+# Creating slice object
+slice = slice(8, -4)
+# This will help us to slice our website variables easily
+
+print(website_1[slice])
+print(website_3[slice])
+print(website_2[slice])
